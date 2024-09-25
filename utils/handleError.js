5f617ded2012540777f1dc2bc4615d7fb62020e8
[nicowulf] <[n.wulfsztat@gmail.com]>
2024-09-25 16:52:50 -0300

    add: handleError

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,5 +1,18 @@
 import { randomUUID } from "node:crypto";
 import { readFileSync, writeFileSync } from "node:fs";
 
+const handleError = (error, path) => {
+  const dbError = JSON.parse(readFileSync(path));
+
+  const newError = {
+    id: randomUUID(),
+    type: error.message,
+    date: new Date().toISOString(),
+  };
+
+  dbError.push(newError);
+  writeFileSync(path, JSON.stringify(dbError));
+  return newError;
+};
 
 export { handleError };

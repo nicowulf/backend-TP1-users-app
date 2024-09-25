ea67e82fe1e63a84d7908a563aa182bba431255f
nicowulf <n.wulfsztat@gmail.com>
2024-09-25 19:16:24 -0300

    add: create & update user object

--- utils/createUserObject.js
+++ utils/createUserObject.js
@@ -1,15 +1,20 @@
-// 1° objetener los argumentos pasador por terminal (que vienen del index)
-// 2° desarrollar las funciones que crean los objetos para añadir un usario y actualizar un usuario
-// 3° aplicar control de errores entorno a las posibilidades de que surja uno
-
 const createUserObject = (args) => {
-  try {
-  } catch (error) {}
+  return {
+    nombre: args[1],
+    apellido: args[2],
+    email: args[3],
+    password: args[4],
+    isLoggedIn: false,
+  };
 };
 
 const createUpdateUserObject = (args) => {
-  try {
-  } catch (error) {}
+  return {
+    nombre: args[2],
+    apellido: args[3],
+    email: args[4],
+    password: args[5],
+  };
 };
 
 export { createUserObject, createUpdateUserObject };
